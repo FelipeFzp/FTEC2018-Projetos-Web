894bf5ce08885815ac6d232a2dad8d42a175ed24
Felipe Panassol Zaniol <felipepzaniol@hotmail.com>
2018-03-19 19:17:37 -0300

    Controller books

---
+++ Ftec.PrimeiroProjetoMVC/PrimeiroProjetoMVC/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroProjetoMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeiroProjetoMVC.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private List<Book> _books = new List<Book>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            ViewData["books"] = _books;
+            return View();
+        }
+
+        [HttpGet("Search/{search}")]
+        public IActionResult Search(string search)
+        {
+            ViewData["books"] = _books.Where(p => p.Name.StartsWith(search));
+            return View();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var bookToRemove =_books.SingleOrDefault(p => p.ID.Equals(id));
+            _books.Remove(bookToRemove);
+
+            ViewData["books"] = _books;
+            return View();
+        }
+
+        [HttpPost()]
+        public IActionResult Add(string name, string author, string description)
+        {
+            _books.Add(Book.Create(name, author, description));
+
+            ViewData["books"] = _books;
+            return View();
+        }
+    }
+}
---
+++ Ftec.PrimeiroProjetoMVC/PrimeiroProjetoMVC/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjetoMVC.Models
+{
+    public class Book
+    {
+        public Guid ID { get; protected set; }
+        public string Name { get; protected set; }
+        public string Author { get; protected set; }
+        public string Description { get; protected set; }
+
+        public static Book Create(string name, string author, string description)
+        {
+            var book = new Book();
+
+            book.ID = Guid.NewGuid();
+            book.Name = name;
+            book.Author = author;
+            book.Description = description;
+
+            return book;
+        }
+    }
+}
